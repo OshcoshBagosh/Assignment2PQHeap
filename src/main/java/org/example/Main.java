bbae70373e235188d932dd898139cf9ea3c9cb66
OshcoshBagosh <oscargabrguerrero@gmail.com>
2025-02-25 11:41:23 -0500

    Save progress before pull

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,7 +19,6 @@ public class Main {
             sortedNextOpen ++;
         }
     }// end - heapsort
-
     public static void main(String[] args) {
         try {
             FileReader fr = new FileReader("players.txt");
